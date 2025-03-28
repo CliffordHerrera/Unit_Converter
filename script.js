e46a6e43cbeec16fbdca2e1dbf6b04296c214532
CliffordHerrera <junkie322@gmail.com>
2025-03-28 14:34:25 +0300

    Add files via upload
    I've added "Unit diss", which transforms a many-digit value into different digits and sums them. Just to be. While it's not finished.

--- script.js
+++ script.js
@@ -13,3 +13,38 @@ if (unitType === "mtokm") {
     resultElem.innerText = `${inputValue} km = ${result.toFixed(4)} miles`;
     }
 }
+
+function rnd() {
+
+}
+
+
+let n;
+function funk2() {
+    n = document.getElementById("given").value;
+    //n = 16;
+    //let ds = document.getElementById("disssum");
+    //ds.innerText = n;
+    return n;
+}
+
+function diss(n) {
+    let ds = document.getElementById("disssum");
+    let dr = document.getElementById("dissres");
+    while(n >= 10) {
+        n = n.toString().split('');
+        if (n.length === 2) {
+            ds.innerText = `${n[0]} + ${n[1]}`;
+        }
+        if (n.length === 3) {
+            ds.innerText = `${n[0]} + ${n[1]} + ${n[2]}`;
+        }
+        if (n.length === 4) {
+            ds.innerText = `${n[0]} + ${n[1]} + ${n[2]} + ${n[3]}`;
+        }
+        //ds.innerText = `${n[0]} + ${n[1]} + ${n[2]}`;
+        n = n.map(Number).reduce((a,b) => a + b);
+        dr.innerText = n;
+    }
+    return n;
+}
